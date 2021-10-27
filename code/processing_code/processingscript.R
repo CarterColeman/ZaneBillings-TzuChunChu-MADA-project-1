d88aedd2a7e772fb54f771b5cc2af59c13a38eb5
Zane Billings <39701545+wz-billings@users.noreply.github.com>
2021-10-26 22:21:05 -0400

    updates scripts to use new data and reran exploratoryscript.R

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -11,7 +11,7 @@ library(tidyverse) # for data wrangling
 library(lubridate) # working with date and time data 
 
 # load data
-dat_orig <- readRDS(here::here("data","raw_data","clean_data.rds"))
+dat_orig <- readRDS(here::here("data","raw_data","new_clean_data.rds"))
 
 
 ######################################
@@ -28,47 +28,47 @@ aggregate(dat_orig[,3:30], by=list(dat_orig$season), FUN = function(x) { sum(is.
 # It looks like 'bmi' and 'date_vaccinated' was not collected in 2014, 2015, and were mostly missing in 2016. Large proportion of 'race2'   
 # was missing in 2019 and some were missing in 2020. Need to recode 'race' column. 
 
-# check race variables
-table(dat_orig$race)
-table(dat_orig$race2)
-table(dat_orig$race, dat_orig$race2)
+# Fixing an issue with race2 variable--this was fixed upstream.
+# # check race variables
+# table(dat_orig$race)
+# table(dat_orig$race2)
+# table(dat_orig$race, dat_orig$race2)
 
 
-white <- c("Polynesian","White","White/ Caucasian","White/Caucaian","White/Caucasian")
-black <- c("Black","Black or African American","Black/African American")
-hisp  <- unique(dat_orig$race)[grep("Hispanic", unique(dat_orig$race))] # extract values that contain "Hispanic"
+# white <- c("Polynesian","White","White/ Caucasian","White/Caucaian","White/Caucasian")
+# black <- c("Black","Black or African American","Black/African American")
+# hisp  <- unique(dat_orig$race)[grep("Hispanic", unique(dat_orig$race))] # extract values that contain "Hispanic"
 
 dat_clean <- dat_orig %>% 
-	# create new variable called race4 that contains four groups
-	mutate(race4 = ifelse(race %in% white, "White",
-												ifelse(race %in% black, "Black", 
-															 ifelse(race %in% hisp, "Hispanic", "Other"))),
-	# create new obesity variable 
-	       obesity = ifelse(bmi >= 30, "Yes", "No")) 
+  # Create a categorical variable for obesity.
+	mutate(obesity = ifelse(bmi >= 30, "Yes", "No")) %>%
+  # Rename "days_since_vac" for backwards compatibility with analysis code
+  # (due to updating dataset after code was written)
+  rename(days_before_vac = days_since_vac)
   
-# create days before vaccination variable 
-REF_DATE <- "-09-01"
+# create days before vaccination variable -- this was also done upstream,
+# after this code was already written.
+# REF_DATE <- "-09-01"
 
-dat_clean2 <- dat_clean %>%
-	tidyr::drop_na(prevactiter, titerincrease) %>%
-	dplyr::mutate(
-		year = factor(season),
-		days_before_vac = lubridate::time_length(
-			x = ifelse(
-				test = is.na(season),
-				yes = NA,
-				no = lubridate::interval(
-					start = lubridate::ymd(paste0(season, REF_DATE)),
-					end = date_vaccinated
-				)
-			),
-			unit = "day"
-		)
-	)
+# dat_clean2 <- dat_clean %>%
+# 	tidyr::drop_na(prevactiter, titerincrease) %>%
+# 	dplyr::mutate(
+# 		year = factor(season),
+# 		days_before_vac = lubridate::time_length(
+# 			x = ifelse(
+# 				test = is.na(season),
+# 				yes = NA,
+# 				no = lubridate::interval(
+# 					start = lubridate::ymd(paste0(season, REF_DATE)),
+# 					end = date_vaccinated
+# 				)
+# 			),
+# 			unit = "day"
+# 		)
+# 	)
 
-
-dat_long <- dat_clean2 %>%
-	# pivot the data. This makes it easier to plot for all strains.
+# pivot the data. This makes it easier to plot for all strains.
+dat_long <- dat_clean %>%
 	tidyr::pivot_longer(
 		ends_with("vaccine_fullname"),
 		names_to = "subtype", values_to = "vaccine_component"
@@ -84,5 +84,5 @@ dat_long <- dat_clean2 %>%
 	)
 
 # Save as RDS
-saveRDS(dat_clean2, here::here("data","processed_data","clean_data.rds"))
+saveRDS(dat_clean, here::here("data","processed_data","clean_data.rds"))
 saveRDS(dat_long, here::here("data","processed_data", "long_data.Rds"))
